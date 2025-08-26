fb8c9890577e2fe631837df2b061e8a99c81845d
DTH235708-PHANNGOCKHOINGUYEN <khoinguyenphan2@gmail.com>
2025-08-26 10:32:04 +0700

    Add files via upload

---
+++ BT_Chuong1.py
@@ -0,0 +1,62 @@
+#VD1
+print('Hello World' )   
+
+#VD2
+print('Albert Einstein đã từng nói: Một người không bao giờ sai là người không bao giờ thử bất kỳ điều gì mới.')
+
+#VD3
+if 5 > 2:
+    print('Five is greater than two!')
+
+#VD4
+total = 1 + \
+2 + \
+3 
+print(total); print('Hello Python')
+
+#VD5
+if True:
+    print('True')
+else:
+    print('False') 
+
+ #VD6
+ #This is a comment
+    print('Hello, World!')      
+
+#BT4
+print('Chao cac ban !')
+
+#BT5
+print('Nguyễn Văn A')
+
+#BT6
+print('Mình về mình có nhớ ta\n' \
+    'Mười lăm năm ấy thiết tha mặn nồng\n' \
+    'Mình về mình có nhớ không?\n' \
+    'Nhìn cây nhớ núi, nhìn sông nhớ nguồn')
+
+#BT8
+print('\n')
+print('* * * *\n')
+print('*     *\n')
+print('*     *\n')
+print('* * * *\n')
+
+#BT9
+print('\n')
+print('* * * * * * * * * * *\n')
+print('* * * * * * * * * * *\n')
+print('* * * * * * * * * * *\n')
+print('* * * * * * * * * * *\n')
+
+#BT10
+print('\n')
+print('            *               \n')
+print('          * * *              \n')
+print('      * * * * * * *            \n')
+print('          * * *              \n')
+print('        * * * * *            \n')
+print('  * * * * * * * * * * *           \n')
+print('          *   *            \n')
+print('          *   *            \n')
